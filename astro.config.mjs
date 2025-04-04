1a39acf5853e0d543794354644c0ad40ecb5556e
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2025-04-05 01:31:20 +0700

    upgrade deps; update theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bhaga.dev',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://bhaga.dev",
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
