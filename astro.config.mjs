476de337b664dd46e8a5beee9b15eacf2b32c2c2
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2025-11-04 03:22:10 +0700

    add search function, wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bhaga.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
   },
